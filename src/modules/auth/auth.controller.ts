7990791ca3532c95b72d0a5fe698b7066f67b132
Ira Marysheva <380934581587a@gmail.com>
2024-08-20 11:48:05 +0300

    Swagger documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,16 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUSerDTO } from '../user/dto';
 import { userLoginDTO } from './dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthUserResponse } from './response';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService:AuthService){}
-
+    @ApiTags('API')
+    @ApiResponse({status:201, type:CreateUSerDTO})
     @Post('register')
     register(@Body() dto:CreateUSerDTO):Promise<CreateUSerDTO>{
         return this.authService.registerUser(dto)
     }
 
+    @ApiTags('API')
+    @ApiResponse({status:200, type:AuthUserResponse})
     @Post('login')
     login(@Body() dto:userLoginDTO):Promise<userLoginDTO>{
         return this.authService.loginUser(dto)
--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,15 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateUSerDTO {
+    @ApiProperty()
     @IsString()
     firstName:string
 
+    @ApiProperty()
     @IsString()
     userName:string
 
+    
+    @ApiProperty()
     @IsString()
     email:string
 
+    @ApiProperty()
     @IsString()
     password:string
 }
