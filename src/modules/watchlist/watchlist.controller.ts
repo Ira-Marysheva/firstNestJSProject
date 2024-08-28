e02f7bbec97e35c063c3b613fc9ced1546da82cb
Ira Marysheva <380934581587a@gmail.com>
2024-08-28 15:04:05 +0300

    New swagger documentation

--- src/modules/watchlist/watchlist.controller.ts
+++ src/modules/watchlist/watchlist.controller.ts
@@ -10,18 +10,24 @@ import {
 import { WatchlistService } from './watchlist.service';
 import { WatchlistDTO } from './dto';
 import { JwtAuthGuard } from 'src/guards/jwt-guard';
+import { CreateAssetResponse } from './response';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('watchlist')
 export class WatchlistController {
   constructor(private readonly watchlistService: WatchlistService) {}
 
+  @ApiTags('API')
+  @ApiResponse({status:201, type:CreateAssetResponse })
   @UseGuards(JwtAuthGuard)
   @Post('create')
-  createAsset(@Body() assetDTO: WatchlistDTO, @Req() request) {
+  createAsset(@Body() assetDTO: WatchlistDTO, @Req() request):Promise<CreateAssetResponse> {
     const user = request.user;
     return this.watchlistService.createAsset(user, assetDTO);
   }
   
+  @ApiTags('API')
+  @ApiResponse({status:200})
   @UseGuards(JwtAuthGuard)
   @Delete('delete')
   deleteAsset(@Query('id') assetId: string, @Req() request):Promise<boolean> {
